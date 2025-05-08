da311c4590eb57c89af1b27b67515e0e9aec3cc3
thunderdc <38782704+thunderdc@users.noreply.github.com>
2025-05-08 21:00:56 +0800

    Update database-start.sh

--- .github/database-start.sh
+++ .github/database-start.sh
@@ -6,6 +6,6 @@ export HOST=$(echo $INFO | jq -r '.database' | jq -r '.host')
 export SERVICE=$(echo $INFO | jq -r '.database' | jq -r '.service')
 export PASSWORD=$(echo $INFO | jq -r '.database' | jq -r '.password')
 
-curl -v -k  GET https://10.4.1.211/ords/testpilot/admin/download_logs/85 -o "aa.txt"
+curl -v -k  GET https://10.4.1.211/ords/testpilot/admin/download_logs/85 > aa.txt
 ls -la /home/ubuntu/actions-runner/_work/standard-test/standard-test
 pwd
