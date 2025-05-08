baed29bcc2ad485145eb39d4de1885a23b8a1104
thunderdc <38782704+thunderdc@users.noreply.github.com>
2025-05-08 20:43:27 +0800

    Update database-start.sh

--- .github/database-start.sh
+++ .github/database-start.sh
@@ -6,4 +6,5 @@ export HOST=$(echo $INFO | jq -r '.database' | jq -r '.host')
 export SERVICE=$(echo $INFO | jq -r '.database' | jq -r '.service')
 export PASSWORD=$(echo $INFO | jq -r '.database' | jq -r '.password')
 
-curl -v -k GET https://10.4.1.211/ords/testpilot/admin/download_logs/85
+# curl -v -k GET https://10.4.1.211/ords/testpilot/admin/download_logs/85
+pwd
