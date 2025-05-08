99eaca6270fd1e076b7f18703430ec72b7b0d2ea
thunderdc <38782704+thunderdc@users.noreply.github.com>
2025-05-08 18:40:19 +0800

    Update database-start.sh

--- .github/database-start.sh
+++ .github/database-start.sh
@@ -6,4 +6,4 @@ export HOST=$(echo $INFO | jq -r '.database' | jq -r '.host')
 export SERVICE=$(echo $INFO | jq -r '.database' | jq -r '.service')
 export PASSWORD=$(echo $INFO | jq -r '.database' | jq -r '.password')
 
-curl -v -s -X GET "https://10.4.1.211/ords/testpilot/admin/database?p_type=autonomous" -H "accept: application/json"
+curl -v -s -X GET "https://z3gk1l7d.adb.us-ashburn-1.oraclecloud.com/ords/testpilot/admin/database?p_type=autonomous" -H "accept: application/json"
