822bbb8c799eeff8c6cc18a465a684a00ce3380d
thunderdc <38782704+thunderdc@users.noreply.github.com>
2025-05-08 22:16:02 +0800

    Update database-start.sh

--- .github/database-start.sh
+++ .github/database-start.sh
@@ -6,5 +6,4 @@ export HOST=$(echo $INFO | jq -r '.database' | jq -r '.host')
 export SERVICE=$(echo $INFO | jq -r '.database' | jq -r '.service')
 export PASSWORD=$(echo $INFO | jq -r '.database' | jq -r '.password')
 
-curl -k GET https://api.testpilot-controller.oraclecloud.com/ords/testpilot/admin/download_logs/41873055428 > /tmp/C.txt
-ls -la /tmp/C.txt
+curl -s -X POST "https://${HOST}.oraclecloudapps.com/ords/admin/_/sql" -H 'content-type: application/sql' -H 'accept: application/json' -basic -u admin:${PASSWORD} --data-ascii "create user test_$RUNID identified by \"My_Password_4_Testing\" DEFAULT TABLESPACE DATA TEMPORARY TABLESPACE TEMP;alter user test_$RUNID quota unlimited on data;grant CREATE SESSION, RESOURCE, CREATE VIEW, CREATE SYNONYM, CREATE ANY INDEX, EXECUTE ANY TYPE to test_$RUNID;"
