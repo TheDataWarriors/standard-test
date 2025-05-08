23f31430c38491f7ba897209f9e0e1e1d504873c
thunderdc <38782704+thunderdc@users.noreply.github.com>
2025-05-08 21:55:14 +0800

    Update database-start.sh

--- .github/database-start.sh
+++ .github/database-start.sh
@@ -6,8 +6,8 @@ export HOST=$(echo $INFO | jq -r '.database' | jq -r '.host')
 export SERVICE=$(echo $INFO | jq -r '.database' | jq -r '.service')
 export PASSWORD=$(echo $INFO | jq -r '.database' | jq -r '.password')
 
-curl -v -k GET https://api.testpilot-controller.oraclecloud.com/ords/testpilot/admin/download_logs/41873055428 > /tmp/B.txt
+# curl -v -k GET https://api.testpilot-controller.oraclecloud.com/ords/testpilot/admin/download_logs/41873055428 > /tmp/B.txt
 # ls -la /home/ubuntu/actions-runner/_work/standard-test/standard-test
-less /home/ubuntu/actions-runner/_work/standard-test/standard-test/B.txt
+# less /home/ubuntu/actions-runner/_work/standard-test/standard-test/B.txt
 # echo "HI" > /tmp/dd.txt
-
+less /tmp/B.txt
