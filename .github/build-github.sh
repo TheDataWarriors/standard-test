8bf0efa046e741b4be9468dd005610ab847b27cf
loic_lefevre <loic.lefevre@oracle.com>
2025-06-04 16:47:20 +0200

    Test #1.0.12

--- .github/build-github.sh
+++ .github/build-github.sh
@@ -1,19 +1,10 @@
-#! /bin/bash
+#!/bin/bash
 
-echo "Managing Oracle Autonomous Database..."
-export INFO=$(curl -s -k -L -X GET "https://api.testpilotdev-controller.oraclecloud.com/ords/testpilot/admin/database?type=autonomous&hostname=`hostname`" -H 'accept: application/json')
-export HOST=$(echo $INFO | jq -r '.database' | jq -r '.host')
-export SERVICE=$(echo $INFO | jq -r '.database' | jq -r '.service')
-goal="-DdbUser=test_$RUNID -DdbPassword=My_Password_4_Testing -DrunID=$RUNID -DdbHost=$HOST -DdbService=$SERVICE"
+goal="-Drdbms=$RDBMS -DdbUser=test_$RUNID -DdbPassword=My_Password_4_Testing -DrunID=$RUNID -DdbHost=$HOST -DdbService=$SERVICE"
 
 function logAndExec() {
   echo 1>&2 "Executing:" "${@}"
   exec "${@}"
 }
-
-echo "DIR: `pwd`"
-echo $PATH
-echo $JAVA_HOME
-ls -palrt ./gradle/
 
 logAndExec ./gradlew test ${goal} "${@}" -Plog-test-progress=true --stacktrace
